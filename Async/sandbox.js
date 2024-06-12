2380f5ae3bc4a5791ef6f66ea9547eb3cd86e5c0
Chartine02 <noellachartine125@gmail.com>
2024-06-12 18:08:19 +0200

    Fetch Api

--- Async/sandbox.js
+++ Async/sandbox.js
@@ -1,11 +1,24 @@
-fetch("todos/luigi.json")
-  .then((response) => {
-    console.log("resolved", response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("rejected", err);
-  });
+const getTodos = async () => {
+  const response = await fetch("todos/luigis.json").then();
+  if (response.status !== 200) {
+    throw new Error("can not fetch the data");
+  }
+  const data = await response.json();
+
+  return data;
+};
+getTodos()
+  .then((data) => console.log("resolved", data))
+  .catch((err) => console.log("rejected:", err.message));
+
+// fetch("todos/luigi.json")
+//   .then((response) => {
+//     console.log("resolved", response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("rejected", err);
+//   });
